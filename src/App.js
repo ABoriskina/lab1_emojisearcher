99582d111e80a7b9a1442e3c228ae9c86f06828b
Alyona Boriskina <99901626+ABoriskina@users.noreply.github.com>
2023-03-23 16:20:09 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,37 +2,51 @@ import './App.css';
 import {useEffect, useState} from "react";
 
 function App() {
-  let [data, setData] = useState([])
 
-  useEffect(() => {
-      fetch('https://emoji-api.com/emojis?access_key=dd44ee51db8369b1fa5a78742276b4f8fc6468e8')
-          .then(res => res.json())
-          .then(res => setData(res))
+    let [data, setData] = useState([])
+    let [search,setSearch]=useState([''])
+
+    useEffect(() => {
+        fetch('https://emoji-api.com/emojis?access_key=dd44ee51db8369b1fa5a78742276b4f8fc6468e8')
+            .then(res => res.json())
+            .then(res => setData(res))
     }, [])
 
-  return (
-    <div className="App">
-      <h1>
-        Emoji Search
-      </h1>
-      <p className="pStyle">
-        Enter the key word
-      </p>
-      <input type="text" placeholder="e.g. smile" className="inputStyle"/>
-      <button>🔎</button>
-        <div className="container">{
-            data.map((e,i) =>
-                <div className="cell">
-                    <p className="emojiStyle">{e.character}</p>
-                    <p className="descriptionStyle">{e.unicodeName}</p>
-                </div>
-            )
-        }
+    let handleSearch = (e) =>{
+        setSearch(e.target.value)
+    }
+
+    let handleSubmit = () =>{
+        console.log("Button is pressed");
+        if (search !== ''){
+            fetch('https://emoji-api.com/emojis?search=' + search +'&access_key=dd44ee51db8369b1fa5a78742276b4f8fc6468e8')
+                .then(res => res.json())
+                .then(res => setData(res))}
+    }
+
+    return (
+        <div className="App">
+            <h1>
+                Emoji Search
+            </h1>
+            <p className="pStyle">
+                Enter the key word
+            </p>
+            <input type="text" placeholder="e.g. smile" className="inputStyle" value={search} onChange={(e)=>handleSearch(e) }/>
+            <button className='search' onClick={handleSubmit}> 🔎</button>
+            <div className="container">{
+                data.map((e,i) =>
+                    <div className="cell" key ={e.slug}>
+                        <p className="emojiStyle">{e.character}</p>
+                        <p className="descriptionStyle">{e.unicodeName}</p>
+                    </div>
+                )
+            }
 
 
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
